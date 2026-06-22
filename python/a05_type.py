4c73b78ac7d446f8567974c1339648f8ae709c72
jungyeon0220 <jungyeon0220@gmail.com>
2026-06-22 16:16:48 +0900

    type

---
+++ python/a05_type.py
@@ -0,0 +1,12 @@
+def main():
+    # 파이썬에서는 변수 선언을 따로 하지 않는다.
+    int_a = 123
+    print(int_a, type(int_a))
+    int_a = "123 숫자"
+    print(int_a, type(int_a))
+    int_a = 3.141592
+    print(int_a, type(int_a))
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/a08_operator.py
@@ -0,0 +1,22 @@
+class Add_test:
+    def __add__(self, other):
+        return "더하기 연산이 실행 되었다"
+
+
+def main():
+    print(2 ** 4)
+    print(2 ** 64)
+    print(18/4)
+    print(type(18/3))
+
+    print(18 // 3)
+    print(type(18 // 3))
+
+    print(14 % 3)
+    a = Add_test()
+    b = Add_test()
+    print(a + b)
+    
+
+if __name__ == "__main__":
+    main()
