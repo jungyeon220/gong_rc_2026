7fc79317dfbdeae509a7ba4b3d9d1ee9718c28a6
jungyeon220 <jungyeon0220@gmail.com>
2026-06-24 16:08:29 +0900

    iterable

---
+++ a45_iterable.py
@@ -0,0 +1,28 @@
+from collections.abc import Iterable
+
+class SimpleIter:
+    def __init__(self, start, end):
+        self.current = start
+        self.end = end
+
+    def __lter__(self):
+        return self    
+    def __next__(self):
+        if self.current >= self.end:
+            raise StopIteration
+        value = self.current 
+        self.current +=1
+        return value
+
+def main():
+   iter = SimpleIter(0, 10)
+   print(isinstance(iter, Iterable))
+   print(isinstance("aa", str))
+   print(isinstance("aa", object))
+   for v in iter:
+       print()
+
+
+
+if __name__ == "__main__":
+    main()
