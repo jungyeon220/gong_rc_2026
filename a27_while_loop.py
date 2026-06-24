0806b5c7834d6093a9afb6e48ae30eaedf602218
jungyeon220 <jungyeon0220@gmail.com>
2026-06-24 09:54:18 +0900

    time_module

--- a27_while_loop.py
+++ a27_while_loop.py
@@ -10,9 +10,14 @@ def main():
             print(".", end="", flush=True)
             time.sleep(0.1)
 
-        
     except KeyboardInterrupt:
         print("키보드 인터럽트!!")
+    
+    list_test = list("choi su gil is python teacher!!")
+    print(list_test)
+    while "s" in list_test:
+        list_test.remove("s")
+    print(list_test)
 
 if __name__ == "__main__":
     main()
