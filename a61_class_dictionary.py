0e313cfd817e9702ffe9fc4ff48c92ee650abee6
jungyeon220 <jungyeon0220@gmail.com>
2026-06-25 15:00:33 +0900

    destructor

---
+++ a61_class_dictionary.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+
+def create_student()
+
+def main():
+    student_path = Path(__file__).parent.parent / "data/student_info.json"
+    with open(student_path, "r", encoding='utf-8') as f:
+        student = json.load(f)
+    print("이름\t총점\t평균")
+    for student in students:
+        score_sum = (student["korean"] * student["math"] * student["english"] * student["science"])
+        score_average = score_sum / 4
+        print(f"(student["name"])\t(score_sum)\t(score_average)")
+
+
+if __name__ == "__main__":
+    main()   
